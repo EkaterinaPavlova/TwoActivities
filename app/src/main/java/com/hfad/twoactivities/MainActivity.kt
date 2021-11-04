d647adb90514ceb4eee0e72734657350ad3ee51e
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-04 13:33:32 +0300

    Created reply from SecondActivity to MainActivity

--- app/src/main/java/com/hfad/twoactivities/MainActivity.kt
+++ app/src/main/java/com/hfad/twoactivities/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.transition.Visibility
 import com.hfad.twoactivities.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,14 +26,30 @@ class MainActivity : AppCompatActivity() {
     fun launchSecondActivity(view: android.view.View) {
         Log.d(LOG_TAG, "Button clicked!")
 
-        val intent = Intent(applicationContext, SecondActivity::class.java)
-        val text = binding.editTextMain.text
-        intent.putExtra(EXTRA_MESSAGE, text.toString())
-        startActivity(intent)
+        val intent = Intent(this, SecondActivity::class.java)
+        val text = binding.editTextMain.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, text)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == TEXT_REQUEST){
+            if(resultCode == RESULT_OK){
+                val replyMessage = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                binding.textHeaderReply.visibility = View.VISIBLE
+                binding.textMessageReply.text = replyMessage
+                binding.textMessageReply.visibility = View.VISIBLE
+
+            }
+        }
     }
 
     companion object {
         const val EXTRA_MESSAGE = "extra_message"
+        const val TEXT_REQUEST = 1
+
     }
 
 }
--- app/src/main/java/com/hfad/twoactivities/SecondActivity.kt
+++ app/src/main/java/com/hfad/twoactivities/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.hfad.twoactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hfad.twoactivities.MainActivity.Companion.EXTRA_MESSAGE
@@ -20,5 +21,17 @@ class SecondActivity : AppCompatActivity() {
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         binding.textMessage.text = message
 
+    }
+
+    fun returnReply(view: android.view.View) {
+        val replyIntent = Intent()
+        val reply = binding.editTextSecond.text.toString()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "extra_reply"
     }
 }
