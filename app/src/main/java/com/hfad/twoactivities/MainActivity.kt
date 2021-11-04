403fc63b89f35222e3564b97cf8322b9d31b602b
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-04 12:05:48 +0300

    Created first activity

---
+++ app/src/main/java/com/hfad/twoactivities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hfad.twoactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity() {
+        Log.d(LOG_TAG, "Button clicked!")
+
+    }
+}
