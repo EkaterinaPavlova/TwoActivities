888f567cdc7effe008b714e415297e2147561ad8
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-04 13:01:10 +0300

    Send message to SecondActivity

--- app/src/main/java/com/hfad/twoactivities/MainActivity.kt
+++ app/src/main/java/com/hfad/twoactivities/MainActivity.kt
@@ -5,19 +5,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.hfad.twoactivities.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val LOG_TAG = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 
     fun launchSecondActivity(view: android.view.View) {
         Log.d(LOG_TAG, "Button clicked!")
 
-        startActivity(Intent(applicationContext, SecondActivity::class.java))
+        val intent = Intent(applicationContext, SecondActivity::class.java)
+        val text = binding.editTextMain.text
+        intent.putExtra(EXTRA_MESSAGE, text.toString())
+        startActivity(intent)
     }
+
+    companion object {
+        const val EXTRA_MESSAGE = "extra_message"
+    }
+
 }
--- app/src/main/java/com/hfad/twoactivities/SecondActivity.kt
+++ app/src/main/java/com/hfad/twoactivities/SecondActivity.kt
@@ -2,13 +2,23 @@ package com.hfad.twoactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hfad.twoactivities.MainActivity.Companion.EXTRA_MESSAGE
+import com.hfad.twoactivities.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        binding = ActivitySecondBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val intent = intent
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        binding.textMessage.text = message
 
     }
 }
