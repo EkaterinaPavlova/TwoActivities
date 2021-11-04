6b8a0666d7c170ff953f3e5b87ab5c07409555da
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-04 12:22:54 +0300

    Created intent

--- app/src/main/java/com/hfad/twoactivities/MainActivity.kt
+++ app/src/main/java/com/hfad/twoactivities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.hfad.twoactivities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,8 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun launchSecondActivity() {
+    fun launchSecondActivity(view: android.view.View) {
         Log.d(LOG_TAG, "Button clicked!")
 
+        startActivity(Intent(applicationContext, SecondActivity::class.java))
     }
 }
